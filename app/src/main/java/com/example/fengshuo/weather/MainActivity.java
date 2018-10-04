1bace8c33f7dea44bc627540cbcc854a3f04e617
fengshuoshuo <690849351@qq.com>
2018-10-05 00:48:32 +0800

    test

--- app/src/main/java/com/example/fengshuo/weather/MainActivity.java
+++ app/src/main/java/com/example/fengshuo/weather/MainActivity.java
@@ -10,6 +10,8 @@ import android.os.Bundle;
 public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.weather_info);
     }
 }
