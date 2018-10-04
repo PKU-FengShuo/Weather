ddf7edfb083b67ee382b578678cb5e6fdc864a1e
fengshuoshuo <690849351@qq.com>
2018-10-04 21:45:37 +0800

    添加资源

---
+++ app/src/main/java/com/example/fengshuo/weather/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.fengshuo.weather;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by fengshuo on 2018/10/4.
+ */
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
