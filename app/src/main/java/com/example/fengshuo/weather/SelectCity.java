d73e51d474188a71fde1bfa70c2b88b73d79f54e
fengshuoshuo <690849351@qq.com>
2018-11-07 22:16:43 +0800

    实验12完成，select_city可搜索

--- app/src/main/java/com/example/fengshuo/weather/SelectCity.java
+++ app/src/main/java/com/example/fengshuo/weather/SelectCity.java
@@ -4,10 +4,14 @@ import android.app.Activity;
 import android.content.Intent;
 import android.nfc.Tag;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -34,7 +38,11 @@ public class SelectCity extends Activity implements View.OnClickListener{
     private String[] data={"第1组","第2组","第3组","第4组","第5组","第6组", "第7组","第8组","第9组","第10组","第11组","第12组","第13组", "第14组","第15组","第16组","第17组","第18组","第19组","第20组",
             "第21组","第22组"};
     private int result=-1;
+    private City ResultCity;
     private String code;
+
+    private EditText mSearchEditText;
+    private SearchCityAdapter mSearchCityAdapter;
 
     final List<String> CityName=new ArrayList<String>();
     final List<String> CityCode=new ArrayList<String>();
@@ -52,8 +60,6 @@ public class SelectCity extends Activity implements View.OnClickListener{
         mCityList=myApplication.getCityList();
         //mCityList=mCityDB.getAllCity();
 
-
-
         for (City city:mCityList){
 
             //Log.d("showcity",city.getCity());
@@ -64,18 +70,53 @@ public class SelectCity extends Activity implements View.OnClickListener{
 
         mlistView=(ListView)findViewById(R.id.list_view);
 
-        ArrayAdapter adapter=new ArrayAdapter(
+        final ArrayAdapter adapter=new ArrayAdapter(
                 this,android.R.layout.simple_list_item_1,CityName);
         mlistView.setAdapter(adapter);
         mlistView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
             @Override
             public void onItemClick(AdapterView<?> adapterView,View view,int i,long l){
-                mTitleName=(TextView)findViewById(R.id.title_name);
-                mTitleName.setText("当前城市："+CityName.get(i));
+                City city;
+                if(mSearchCityAdapter!=null){
+                    city=(City) mSearchCityAdapter.getItem(i);
+                }
+                else
+                    city=mCityList.get(i);
                 result=i;
-                Toast.makeText(SelectCity.this,"你单击了："+i,Toast.LENGTH_SHORT).show();
+                ResultCity=city;
+                mTitleName=(TextView)findViewById(R.id.title_name);
+                mTitleName.setText("当前城市："+city.getCity());
+                Toast.makeText(SelectCity.this,"你单击了："+i+"citycode:"+city.getNumber(),Toast.LENGTH_SHORT).show();
             }
         });
+
+        TextWatcher mTextWatcher=new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                mSearchCityAdapter=new SearchCityAdapter(SelectCity.this,mCityList);
+
+                mlistView.setTextFilterEnabled(true);
+                if (mCityList.size()<1|| TextUtils.isEmpty(s)){
+                    mlistView.setAdapter(adapter);
+                }else {
+                    mlistView.setAdapter(mSearchCityAdapter);
+                    mSearchCityAdapter.getFilter().filter(s);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        };
+
+        mSearchEditText=(EditText)findViewById(R.id.search_edit);
+        mSearchEditText.addTextChangedListener(mTextWatcher);
     }
 
     @Override
@@ -83,8 +124,8 @@ public class SelectCity extends Activity implements View.OnClickListener{
         switch (v.getId()){
             case R.id.title_back:
                 Intent i=new Intent();
-                if (result>0){
-                    code=CityCode.get(result);
+                if (result>=0){
+                    code=ResultCity.getNumber();
                     i.putExtra("cityCode",code);
                 }
                 else {
